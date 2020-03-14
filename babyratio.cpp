dd5638e0485cdece73d485ead77e3dfbeac02310
nikolaos-koufalexis <61936706+nikolaos-koufalexis@users.noreply.github.com>
2020-03-14 16:34:50 +0200

    Create babyratio.cpp

---
+++ babyratio.cpp
@@ -0,0 +1,77 @@
+#ifndef CONTEST  
+#include "babyratio.hpp"
+#endif  
+#include <iostream>  
+  
+using namespace std;  
+  
+rational::rational (int n,int d){  
+  nom=n;  
+  den=d;  
+  int gcd=rational::gcd(nom,den);  
+  den/=gcd;  
+  nom/=gcd;  
+}  
+  
+rational rational::add(rational r){  
+  r.nom=r.nom*den+r.den*nom;  
+  r.den=r.den*den;  
+  int gcd=rational::gcd(r.nom,r.den);  
+  r.den/=gcd;  
+  r.nom/=gcd;  
+  return r;  
+}  
+  
+rational rational::sub(rational r){  
+  r.nom=-(r.nom*den)+(r.den*nom);  
+  r.den=r.den*den;  
+  int gcd=rational::gcd(r.nom,r.den);  
+  r.den/=gcd;  
+  r.nom/=gcd;  
+  return r;  
+}  
+  
+rational rational::mul(rational r){  
+  r.nom=r.nom*nom;  
+  r.den=r.den*den;  
+  int gcd=rational::gcd(r.nom,r.den);  
+  r.den/=gcd;  
+  r.nom/=gcd;  
+  return r;  
+}  
+  
+  
+rational rational::div(rational r){  
+  r.nom=r.nom*den;    
+  r.den=r.den*nom;  
+  int gcd=rational::gcd(r.nom,r.den);  
+  r.den/=gcd;  
+  int temp=r.den;  
+  r.nom/=gcd;  
+  r.den=r.nom;  
+  r.nom=temp;  
+  return r;  
+}  
+  
+void rational::print(){
+  nom=den<0 ? -nom : nom;
+  den=den<0 ? -den : den;
+  cout<<nom<<"/"<<den;  
+}  
+  
+int rational::gcd(int a,int b){  
+  
+  if (b == 0){  
+    return a;  
+  }  
+  else{  
+    return gcd(b, a % b);  
+  }  
+}  
+
+int main(){
+	rational a(3,8);
+	rational b(2,-11);
+	
+	a.div(b).print();
+}
